a528eca22ea8b208584795998292de5660543b58
ItsGJK <gabrieljameskeller@gmail.com>
2022-01-03 11:39:34 -0600

    switch back to downloading uncompressed cif files

--- src/cif_downloader.py
+++ src/cif_downloader.py
@@ -15,6 +15,6 @@ def download_from_file(file_name):
 
 
 def download_cif(pdb_id):
-    request = requests.get(f"https://files.rcsb.org/download/{pdb_id}.cif.gz", allow_redirects=True)
+    request = requests.get(f"https://files.rcsb.org/download/{pdb_id}.cif", allow_redirects=True)
     with open(f"resources/cif/{pdb_id}.cif.gz", "wb+") as file:
         file.write(request.content)
