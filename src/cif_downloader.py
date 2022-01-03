f05c170105bc30127e3e77d3a18c26cd4923ef4a
ItsGJK <gabrieljameskeller@gmail.com>
2022-01-03 11:42:12 -0600

    writing uncompressed files (bugfix)

--- src/cif_downloader.py
+++ src/cif_downloader.py
@@ -16,5 +16,5 @@ def download_from_file(file_name):
 
 def download_cif(pdb_id):
     request = requests.get(f"https://files.rcsb.org/download/{pdb_id}.cif", allow_redirects=True)
-    with open(f"resources/cif/{pdb_id}.cif.gz", "wb+") as file:
+    with open(f"resources/cif/{pdb_id}.cif", "wb+") as file:
         file.write(request.content)
